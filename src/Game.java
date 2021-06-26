753fdf046c208da3d7ea60b22772a67a64cd4734
littlejoem <malyshkz79@gmail.com>
2021-06-26 07:42:08 +0200

    Create Puzzle Image vid#3 5:33

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,14 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Game extends JFrame {
     private ArrayList<Point> solution;
+    private ArrayList<PuzzleButton> buttons;
     private JPanel panel;
     private BufferedImage source;
     private BufferedImage resized;
@@ -17,13 +21,21 @@ public class Game extends JFrame {
 
     public void initUI() {
         solution = new ArrayList<>();
-        int line = 3;
-        int row = 2;
-        for (int i = 0; i < line; i++) {
-            for (int j = 0; j < row; j++) {
-                solution.add(new Point(line, row ));
+        int row = 3;
+        int col = 2;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                solution.add(new Point(row, col ));
             }
         }
+        buttons = new ArrayList<>();
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createLineBorder(Color.gray));
+        panel.setLayout(new GridLayout(4,3));
+    }
 
+    public BufferedImage loadImage() throws IOException {
+        BufferedImage bimg = ImageIO.read(new File("scale_1200.jpg"));
+        return bimg;
     }
 }
