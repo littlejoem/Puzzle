3c41ab093870a2860f2e4b4c531042e8c2490dc4
littlejoem <malyshkz79@gmail.com>
2021-07-05 06:33:05 +0200

    Image cut to puzzle vid#4 ended

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,8 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.CropImageFilter;
+import java.awt.image.FilteredImageSource;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,6 +16,8 @@ public class Game extends JFrame {
     private BufferedImage resized;
     private int width, height;
     private final int DESIRED_WIDTH = 400;
+    private Image image;
+    private PuzzleButton lastButton;
 
     public Game() {
         initUI();
@@ -38,7 +42,29 @@ public class Game extends JFrame {
             resized = resizeImage(source, DESIRED_WIDTH, h, BufferedImage.TYPE_INT_ARGB);
 
         } catch (IOException ex) {
-            System.err.println("Problem this source image " + ex);
+            System.err.println("Problem with source image " + ex);
+        }
+
+        width = resized.getWidth();
+        height = resized.getHeight();
+        add (panel, BorderLayout.CENTER);
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                image = createImage(new FilteredImageSource(resized.getSource(),
+                        new CropImageFilter(j * width / 3, i * height / 4, width / 3, height / 4)));
+                PuzzleButton button = new PuzzleButton(image);
+                button.putClientProperty("position", new Point(i, j));
+
+                if (i == 4 && j == 3) {
+                    lastButton = new PuzzleButton();
+                    lastButton.setBorderPainted(false);
+                    lastButton.setContentAreaFilled(false);
+                    lastButton.setLastButton(true);
+                } else {
+                    buttons.add(button);
+                }
+            }
         }
     }
 
